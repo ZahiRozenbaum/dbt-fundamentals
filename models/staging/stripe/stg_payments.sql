bcf4f458ceee96b218362335d5badc29a8347405
zrozenbaum@yahoo.com <zrozenbaum@yahoo.com>
2022-05-17 12:00:06 +0000

    add macro limit_data_in_dev.sql

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -1,6 +1,14 @@
+with payment as
+(
+    select *  from {{source('stripe','payment')}}
+    {{limit_data_in_dev('_BATCHED_AT', 30)}}
+)
+
+
 select orderid as order_id,status,PAYMENTMETHOD
 , {{cents_to_dollars(column_name = 'amount',num_dec = 2 )}}
- from {{source('stripe','payment')}}
+ from payment
 where status='success'
 group by orderid,status,PAYMENTMETHOD
 order by order_id
+
